7fc8abad51ccee9e38fbe1dd8f63dbc8549b0703
joschonarth <joschonarth@gmail.com>
2024-11-23 20:26:31 -0300

    feat: create route to upload an image

--- src/controller/posts-controller.js
+++ src/controller/posts-controller.js
@@ -1,4 +1,5 @@
 import { getTodosPosts, criarPost} from "../models/posts-model.js";
+import fs from 'fs'
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
@@ -9,6 +10,24 @@ export async function postarNovoPost(req, res) {
     const novoPost = req.body;
     try {
         const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na requisição" });        
+    } 
+};
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
         res.status(200).json(postCriado);
     } catch (erro) {
         console.error(erro.message);
--- src/routes/posts-routes.js
+++ src/routes/posts-routes.js
@@ -1,11 +1,24 @@
 import express from 'express';
-import { listarPosts, postarNovoPost } from '../controller/posts-controller.js';
+import multer from 'multer';
+import { listarPosts, postarNovoPost, uploadImagem } from '../controller/posts-controller.js';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+
+const upload = multer({dest: "./uploads", storage})
 
 const routes = (app) => {
     app.use(express.json());
     
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
+    app.post("/posts/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
